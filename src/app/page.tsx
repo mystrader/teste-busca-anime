b58f152f07a6dab0ac916c8bea6e9a6f24e7024f
marcus_fazenda_eds <marcus.cunha@fazenda.gov.br>
2024-04-28 09:47:14 -0300

    :wrench: chore: Configurando prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ import Header from './components/Header';
 export default function Home() {
   return (
     <main className={styles.main}>
+
        <Header />
+      <div className={styles.container}>
+testes
+       </div>
 
     </main>
   );
