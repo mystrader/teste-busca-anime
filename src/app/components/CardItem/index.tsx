c2265d8e08690dba7a9708cbb9bd3f25291e4399
marcus_fazenda_eds <marcus.cunha@fazenda.gov.br>
2024-04-28 19:46:20 -0300

    :sparkles: feat: componente card parte 2 - sem mock

--- src/app/components/CardItem/index.tsx
+++ src/app/components/CardItem/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AverageScore, Tag } from '../shared';
 import styles from './Card.module.css';
 
 interface CardProps {
@@ -12,6 +13,27 @@ const Card: React.FC<CardProps> = ({ imageUrl, title }) => {
       <div className={styles.overlay}></div>
       <img src={imageUrl} alt={title} className={styles.image} />
       <h3 className={styles.title}>{title}</h3>
+
+      <ul className={styles.genreList}>
+        <li>
+          <Tag name="Action" />
+        </li>
+        <li>
+          <Tag name="Adventury" />
+        </li>
+        <li>
+          <Tag name="Commedy" />
+        </li>
+        <li>
+          <Tag name="Drama" />
+        </li>
+        <li>
+          <Tag name="Fantasy" />
+        </li>
+      </ul>
+      <div className={styles.averageScore}>
+        <AverageScore numberScore="81" />
+      </div>
     </div>
   );
 };
--- src/app/utils/font.tsx
+++ src/app/utils/font.tsx
@@ -1,11 +1,8 @@
-import {
-    Mulish,
-} from 'next/font/google';
-  
+import { Mulish } from 'next/font/google';
 
-  export const mulish = Mulish({
-    subsets: ['latin'],
-    weight: ['300', '400', '700'],
-    variable: '--font-mulish',
-    display: 'swap',
-  });
+export const mulish = Mulish({
+  subsets: ['latin'],
+  weight: ['300', '400', '600', '700'],
+  variable: '--font-mulish',
+  display: 'swap',
+});
